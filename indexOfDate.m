54bf4459fb93348063365f9dfbe55fd5d5c9723c
Luigi Catello <62564969+louis1642@users.noreply.github.com>
2023-07-03 17:47:56 +0200

    formato data uuuu-MM-dd

--- indexOfDate.m
+++ indexOfDate.m
@@ -1,7 +1,7 @@
 function y = indexOfDate(dates, elem)
 % y = indexOfDate(dates, elem)
 % Trova l'indice nell'array dates della data elem.
-    y = find(dates == datetime(elem, 'InputFormat', 'MM/dd/uuuu'));
+    y = find(dates == datetime(elem, 'InputFormat', 'uuuu-MM-dd'));
     if (isempty(y)) 
         error('Data ' + string(elem) + ' non trovata');
     end
