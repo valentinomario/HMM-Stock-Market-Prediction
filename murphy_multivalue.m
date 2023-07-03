fe0e0740f971a2ab63fe75b616d9ce0512242c23
ludruggiero <131437995+ludruggiero@users.noreply.github.com>
2023-07-03 17:36:31 +0200

    Cambio variabili

--- murphy_multivalue.m
+++ murphy_multivalue.m
@@ -5,9 +5,9 @@ load Data.mat;  % Date Open Close High Low
 
 % TUTTE LE DATE SONO NEL FORMATO MM/DD/YYYY
 % selezioniamo un periodo di osservazione
-llim = indexOfDate(Date,'01/03/2008');
+llim = indexOfDate(Date,'03/01/2012');
 ulim = indexOfDate(Date,'01/02/2020');
-train_size = 3000;
+train_size = 365;
 Date_l = Date(llim:ulim);
 fracChange = (Open(llim:ulim) - Close(llim:ulim))./Open(llim:ulim);
 fracHigh = (High(llim:ulim) - Close(llim:ulim))./Open(llim:ulim);
@@ -24,32 +24,38 @@ mixturesNumber = 5; % Number of mixture components for each state
 latency = 10; % days aka vectors in sequence
 
 % Markov Chain guesses
-P = 1/underlyingStates.*ones(1, underlyingStates); % initial probabilities of the states
-A = 1/underlyingStates.*ones(underlyingStates, underlyingStates); % transition matrix
+initialProb = 1/underlyingStates.*ones(1, underlyingStates); % initial probabilities of the states
+transitionMatrix = 1/underlyingStates.*ones(underlyingStates, underlyingStates); % transition matrix
 
 observations_train(:,1) = discretize(observations_train(:,1),500);
 observations_train(:,2) = discretize(observations_train(:,2),100);
 observations_train(:,3) = discretize(observations_train(:,3),100);
 obs_tr_t = prepareSequenceTensor(observations_train, latency);
 [mu0, Sigma0, weights] = mixgauss_init(underlyingStates*mixturesNumber, obs_tr_t, 'full');
+
 mu0 = reshape(mu0, [cofficientPerVector underlyingStates mixturesNumber]);
 Sigma0 = reshape(Sigma0, [cofficientPerVector cofficientPerVector underlyingStates mixturesNumber]);
 %mixmat0 = mk_stochastic(rand(underlyingStates, mixturesNumber));
-mixmat0 = reshape(weights,[underlyingStates mixturesNumber]);
+%mixmat0 = reshape(weights,[underlyingStates mixturesNumber]);
+mixmat0 = 1/mixturesNumber.*ones(underlyingStates, mixturesNumber);
 
-[LL, prior1, transmat1, mu1, Sigma1, mixmat1] = mhmm_em(obs_tr_t, P, A, mu0, Sigma0, mixmat0, 'max_iter', 100);
+[LL, prior1, transmat1, mu1, Sigma1, mixmat1] = mhmm_em(obs_tr_t, initialProb, transitionMatrix, mu0, Sigma0, mixmat0, 'max_iter', 15);
 
-figure
-plot(Date_l,Close(llim:ulim)), grid;
-figure
-subplot(3,1,1)
-bar(Date_l,fracChange), grid
-title('Frac Change')
-subplot(3,1,2)
-bar(Date_l,fracHigh), grid
-title('Frac High')
-subplot(3,1,3)
-bar(Date_l,fracLow), grid
-title('Frac Low')
+
+
+%figure
+%plot(Date_l,Close(llim:ulim)), grid;
+%hold on 
+%xline(train_size);
+%figure
+%subplot(3,1,1)
+%bar(Date_l,fracChange), grid
+%title('Frac Change')
+%subplot(3,1,2)
+%bar(Date_l,fracHigh), grid
+%title('Frac High')
+%subplot(3,1,3)
+%bar(Date_l,fracLow), grid
+%title('Frac Low')
 
 
