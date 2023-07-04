b4a8f9c1e081f4059d59185e6637a76a91276fee
Mario Valentino <valentinomario05@gmail.com>
2023-07-04 09:49:03 +0200

    Stima iniziale matrice di emissione

--- hmm_multivalue.m
+++ hmm_multivalue.m
@@ -13,10 +13,12 @@ fracChange = (Open(llim:ulim) - Close(llim:ulim))./Open(llim:ulim);
 fracHigh = (High(llim:ulim) - Close(llim:ulim))./Open(llim:ulim);
 fracLow = (Open(llim:ulim) - Low(llim:ulim))./Open(llim:ulim);
 
+continuos_observations3D = [fracChange, fracHigh, fracLow];
+
 numberOfPoints = [50 10 10];
-[fracChange, edgesFChange] = discretize(fracChange, numberOfPoints(1));
-[fracHigh, edgesFHigh] = discretize(fracHigh, numberOfPoints(2));
-[fracLow, edgesFLow] = discretize(fracLow, numberOfPoints(3));
+[fracChange, edgesFChange] = discretize(fracChange, numberOfPoints(1)-1);
+[fracHigh, edgesFHigh] = discretize(fracHigh, numberOfPoints(2)-1);
+[fracLow, edgesFLow] = discretize(fracLow, numberOfPoints(3)-1);
 
 observations3D = [fracChange, fracHigh, fracLow];
 
@@ -33,12 +35,40 @@ latency = 10; % days aka vectors in sequence
 initialProb = 1/underlyingStates.*ones(1, underlyingStates); % initial probabilities of the states
 transitionMatrix = 1/underlyingStates.*ones(underlyingStates, underlyingStates); % transition matrix
 
-gm3D = fitgmdist(observations3D, mixturesNumber*underlyingStates, 'CovarianceType', 'diagonal', 'RegularizationValue', 1e-10, 'Replicates', 10);
-gm1D = fitgmdist(observations, mixturesNumber*underlyingStates, 'CovarianceType', 'diagonal', 'RegularizationValue', 1e-10, 'Replicates', 10);
+gm3D = fitgmdist(continuos_observations3D, mixturesNumber*underlyingStates, 'CovarianceType', 'diagonal', 'RegularizationValue', 1e-10, 'Replicates', 10);
+%gm1D = fitgmdist(observations, mixturesNumber*underlyingStates, 'CovarianceType', 'diagonal', 'RegularizationValue', 1e-10, 'Replicates', 10);
 
-% TODO prossimi passi:
-% mappare le tre gaussiane nel nuovo spazio 1D
-% ecc
+emissionProbabilities = zeros(underlyingStates,5000);
+
+for i = 1:underlyingStates
+    gm_s{i}= gmdistribution(gm3D.mu((1+(i-1)*mixturesNumber):(i*mixturesNumber),:), ...
+        gm3D.Sigma(1,:,(1+(i-1)*mixturesNumber):(i*mixturesNumber)));
+    for x=edgesFChange
+        for y=edgesFHigh
+            for z=edgesFLow
+                p = pdf(gm_s{i},[x' y' z']);
+                   
+                x_d = find(edgesFChange==x);
+                y_d = find(edgesFHigh==y);
+                z_d = find(edgesFLow==z);
+                n = map3DTo1D(x_d,y_d,z_d,numberOfPoints(1),numberOfPoints(2));
+
+                emissionProbabilities(i,n) = p;
+            end
+        end
+    end
+    emissionProbabilities(i,:) = emissionProbabilities(i,:)./sum(emissionProbabilities(i,:));
+end
+
+%observations_train = convertVectorToCellArray(observations', latency);
+
+observations_train = zeros(length(Date_l)-latency,latency);
+
+for i=1:(length(Date_l)-latency)
+    observations_train(i,:) = observations(i:(i+latency-1));
+end
+
+[ESTTR,ESTEMIT] = hmmtrain(observations(startIndex:endIndex), transitionMatrix, emissionProbabilities,'Verbose',true);
 
 
 
