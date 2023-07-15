9a08784531ec86cf8af4bee7a3510bc957860c73
Luigi Catello <62564969+louis1642@users.noreply.github.com>
2023-07-15 13:32:18 +0200

    Plotting nel main, stampo riepilogo
    TODO allineare axis

--- init.m
+++ init.m
@@ -1,7 +1,7 @@
 disp("Init")
 
-stock_name="AAPL.mat";
-load(stock_name);% Date Open Close High Low
+stock_name = "AAPL.mat";
+load(stock_name);       % Date Open Close High Low
 
 TRAIN = 0;              % see train section: if 0 a specified .mat file is loaded
                         %                    if 1 a new training is done
@@ -11,8 +11,8 @@ shiftWindowByOne = 0;   % see sequences train section: if 0 a new sequence is gr
                         % select period of observation, date format YYYY-MM-DD
 startTrainDate = '2019-01-03';
 endTrainDate = '2022-01-03';
-startTrainDateIdx = indexOfDate(Date,startTrainDate);
-endTrainDateIdx = indexOfDate(Date,endTrainDate);
+startTrainDateIdx = indexOfDate(Date, startTrainDate);
+endTrainDateIdx = indexOfDate(Date, endTrainDate);
 
 trainIndexes = startTrainDateIdx:endTrainDateIdx;
 
@@ -23,7 +23,7 @@ trainIndexes = startTrainDateIdx:endTrainDateIdx;
 useDynamicEdges = 0;
 
 startPredictionDate = '2023-01-03';
-startPredictionDateIdx = indexOfDate(Date,startPredictionDate); % first day of prediction
+startPredictionDateIdx = indexOfDate(Date, startPredictionDate); % first day of prediction
 
 endPredictionDate = Date(end);
 endPredictionDateIdx  = indexOfDate(Date, endPredictionDate);   % last avaiable date
@@ -34,7 +34,7 @@ predictionIndexes = startPredictionDateIdx:endPredictionDateIdx;
 discretizationPoints = [50 10 10];    % uniform intervals to discretize observed parameters
 totalDiscretizationPoints = discretizationPoints(1)*discretizationPoints(2)*discretizationPoints(3);
 
-if(~TRAIN)
+if (~TRAIN)
     filename = ("train/hmmtrain-2023-07-14-00-34-47.mat");
     load(filename);
 end
@@ -42,3 +42,5 @@ end
 underlyingStates = 4; % number of hidden states
 mixturesNumber = 4;   % number of mixture components for each state
 latency = 10;         % days aka vectors in sequence
+
+
--- main.m
+++ main.m
@@ -19,9 +19,9 @@ if exist('edgesFChange','var')==0
         edgesFHigh = dynamicEdges((High(startTrainDateIdx:end) - Open(startTrainDateIdx:end))./Open(startTrainDateIdx:end), discretizationPoints(2));
         edgesFLow = dynamicEdges((Open(startTrainDateIdx:end) - Low(startTrainDateIdx:end))./Open(startTrainDateIdx:end), discretizationPoints(3));
     else
-        edgesFChange = linspace(-0.1,0.1,discretizationPoints(1)+1);
-        edgesFHigh = linspace(0,0.1,discretizationPoints(2)+1);
-        edgesFLow = linspace(0,0.1,discretizationPoints(3)+1);
+        edgesFChange = linspace(-0.1, 0.1, discretizationPoints(1)+1);
+        edgesFHigh = linspace(0, 0.1, discretizationPoints(2)+1);
+        edgesFLow = linspace(0, 0.1, discretizationPoints(3)+1);
     end
 end
 
@@ -33,7 +33,7 @@ end
 % discretized sequences mapped into a monodimensional array
 discreteObservations1D = zeros(length(trainIndexes), 1);
 for i = 1:length(trainIndexes)
-    discreteObservations1D(i) = map3DTo1D(fracChange(i), fracHigh(i), fracLow(i), discretizationPoints(1), discretizationPoints(2),discretizationPoints(3));
+    discreteObservations1D(i) = map3DTo1D(fracChange(i), fracHigh(i), fracLow(i), discretizationPoints(1), discretizationPoints(2), discretizationPoints(3));
 end
 
 %% Markov Chain guesses
@@ -44,9 +44,9 @@ if (TRAIN)
     gaussianMixture = fitgmdist(continuousObservations3D, mixturesNumber*underlyingStates, 'CovarianceType', 'diagonal', 'RegularizationValue', 1e-10, 'Replicates', 10);
     
     % gaussin sorting
-    sortedMu = zeros(mixturesNumber*underlyingStates,3);
+    sortedMu = zeros(mixturesNumber*underlyingStates, 3);
     [sortedMu(:,1), sortedMuIndexes] = sort(gaussianMixture.mu(:,1), 1);
-    sortedMu(:,2:3) = gaussianMixture.mu(sortedMuIndexes,2:3);    % sigma sorting 
+    sortedMu(:,2:3) = gaussianMixture.mu(sortedMuIndexes, 2:3);    % sigma sorting 
     sortedSigma = gaussianMixture.Sigma(1, 1:3, sortedMuIndexes);
     
     % emission probabilities initialized to zeros
@@ -83,7 +83,7 @@ end
 % construction of matrix observations_train containing train sequences of
 % discretized monodimensional values
 
-if(TRAIN)
+if (TRAIN)
     if (shiftWindowByOne) % interval shifted by #days = 1
         totalTrainSequences = length(trainIndexes) - latency + 1;
         trainingSet = zeros(totalTrainSequences, latency);
@@ -95,7 +95,7 @@ if(TRAIN)
     else            % interval shifted by #days = latency
         % last sequence is ignored if length(trainIndexes) mod latency ~=0
         totalTrainSequences = floor(length(trainIndexes) / latency);
-        trainingSet = zeros(totalTrainSequences, latency); %#ok<UNRCH>
+        trainingSet = zeros(totalTrainSequences, latency);
         for i = 1:totalTrainSequences
            startWindowIdx = (i - 1) * latency + 1;
            endWindowIdx = startWindowIdx + latency - 1;
@@ -106,14 +106,14 @@ end
 
 %% train
 if (TRAIN)
-    disp("Train")      %#ok<UNRCH>
+    disp("Train")
     maxIter = 1000;
     trainInfo = struct('maxIter', maxIter, 'converged', 0, 'trainingTime', -1);
     lastwarn('', '');
 
-    tic
+    tic     % start measuring time
     [ESTTR, ESTEMIT] = hmmtrain(trainingSet,transitionMatrix,emissionProbabilities,'Verbose',true,'Maxiterations',maxIter);
-    trainInfo.trainingTime = toc;
+    trainInfo.trainingTime = toc;   % stop measuring time
     [warnMsg, warnId] = lastwarn();
     
     trainInfo.converged = isempty(warnId);
@@ -135,16 +135,19 @@ predictedClose = zeros(predictionLength, 1);
 for currentPrediction = 1:predictionLength
     % historical window indexes
     currentWindowStartIdx = startPredictionDateIdx - latency + currentPrediction;
-    currentWindowEndIdx = startPredictionDateIdx -2 + currentPrediction;
+    currentWindowEndIdx = startPredictionDateIdx - 2 + currentPrediction;
     
     % index corresponding to current predicion in Date vector
-    currentPredictionIdx = currentWindowEndIdx +1;
+    currentPredictionIdx = currentWindowEndIdx + 1;
     currentWindowIndexes = currentWindowStartIdx:currentWindowEndIdx;
+    % notice that, since the HMM was trained on sequences of length 
+    % "latency", we create the currentWindow with "latency - 1"
+    % observations, so that the network will predict the 10th
     
     % historical window observations
-    currentWindowFracChange = (Close(currentWindowIndexes) - Open(currentWindowIndexes))./Open(currentWindowIndexes);
-    currentWindowFracHigh   = (High(currentWindowIndexes) - Open(currentWindowIndexes)) ./Open(currentWindowIndexes);
-    currentWindowFracLow    = (Open(currentWindowIndexes) - Low(currentWindowIndexes))  ./Open(currentWindowIndexes);
+    currentWindowFracChange = (Close(currentWindowIndexes) - Open(currentWindowIndexes)) ./Open(currentWindowIndexes);
+    currentWindowFracHigh   = (High(currentWindowIndexes)  - Open(currentWindowIndexes)) ./Open(currentWindowIndexes);
+    currentWindowFracLow    = (Open(currentWindowIndexes)  - Low(currentWindowIndexes))  ./Open(currentWindowIndexes);
     
     % discretization of historical data used for prediction
     [currentWindowFracChange, ~] = discretize(currentWindowFracChange, edgesFChange);
@@ -156,14 +159,14 @@ for currentPrediction = 1:predictionLength
         currentWindow1D(i) = map3DTo1D(currentWindowFracChange(i), currentWindowFracHigh(i), currentWindowFracLow(i), ...
             discretizationPoints(1), discretizationPoints(2), discretizationPoints(3));
     end
-    %prediction
+    % prediction
     fprintf("%.2f%% : ", currentPrediction / predictionLength * 100);
     predictedObservation1D = hmmPredictObservation(currentWindow1D, ESTTR, ESTEMIT, 'verbose', 1, 'possibleObservations', 1:totalDiscretizationPoints);
     
     if (~isnan(predictedObservation1D))
         % 3D mapping of current valid prediction
         [predictedFracChange, predictedFracHigh, predictedFracLow] = map1DTo3D(predictedObservation1D, discretizationPoints(1), discretizationPoints(2), discretizationPoints(3));
-        % t-th row filled with current 3D valid prediction
+        % currentPrediction-th row filled with current 3D valid prediction
         predictedObservations3D(currentPrediction,:) = [edgesFChange(predictedFracChange), edgesFHigh(predictedFracHigh), edgesFLow(predictedFracLow)];
         
         predictedClose(currentPrediction) = Open(currentPredictionIdx)*(1 + predictedObservations3D(currentPrediction,1));
@@ -174,6 +177,122 @@ for currentPrediction = 1:predictionLength
     end
 
 end
+
+%% Plots
+disp("plots")
+
+% initialization of MAPE
+MAPE = 0;
+fig_candlestick = figure(Name='Candlestick', Position=[500 100 700 420]);
+grid on
+% candlestick plot of actual values
+RG_candle(timetable(Date(predictionIndexes), Open(predictionIndexes), High(predictionIndexes), Low(predictionIndexes), Close(predictionIndexes), 'VariableNames', {'Open', 'High', 'Low', 'Close'}));
+hold on
+% dots of predictions (green: earned money)
+whichDotsAreGreen = sign(predictedClose - Open(predictionIndexes)) == sign(Close(predictionIndexes) - Open(predictionIndexes));
+plot(Date(predictionIndexes(whichDotsAreGreen)), predictedClose(whichDotsAreGreen), '.', MarkerSize=20, Color="#378333");     % green dots
+plot(Date(predictionIndexes(~whichDotsAreGreen)), predictedClose(~whichDotsAreGreen), '.', MarkerSize=20, Color="#A80303");     % green dots
+hold off
+
+fig_closePlot = figure(Name='Real vs predicted Close values', Position=[500 560 700 420]);
+realDataPlot = plot(Date(predictionIndexes), Close(predictionIndexes), "LineWidth", 0.3, "Marker", '.', "MarkerSize", 5);
+grid on
+hold on
+
+predictionInfo = struct('good', 0, 'bad', 0, 'invalid', 0);
+
+% instantiate trading simulation array
+investmentSimulation = 100 .* ones(1, predictionLength + 1);
+% each day the dummy investor decides how to invest -> its capital is
+% calculated for the day after according to the correctness of the guess
+
+% instantiate graphic objects array
+predClosePlot = gobjects(predictionLength, 1);
+
+for currentPrediction = 1:predictionLength
+    % index of the date for the current prediction
+    currentPredictionDateIdx = startPredictionDateIdx - 1 + currentPrediction;
+    % dummy investor
+    if (isnan(predictedClose(currentPrediction)))
+        investmentSimulation(currentPrediction + 1) = investmentSimulation(currentPrediction);
+    else
+        currentFracChange = (Close(currentPredictionDateIdx) - Open(currentPredictionDateIdx))./Open(currentPredictionDateIdx);
+        % long
+        if predictedClose(currentPrediction) > Open(currentPredictionDateIdx)
+            investmentSimulation(currentPrediction + 1) = (1 + currentFracChange) * investmentSimulation(currentPrediction);
+            fprintf("%s: buy for %.2f, sell for %.2f\n", string(Date(currentPredictionDateIdx)), Open(currentPredictionDateIdx), Close(currentPredictionDateIdx));
+        % short
+        elseif predictedClose(currentPrediction) < Open(currentPredictionDateIdx)
+            investmentSimulation(currentPrediction + 1) = (1 - currentFracChange) * investmentSimulation(currentPrediction);
+            fprintf("%s: sell for %.2f, buy for %.2f\n", string(Date(currentPredictionDateIdx)), Open(currentPredictionDateIdx), Close(currentPredictionDateIdx));
+        % if = don't invest
+        else
+            investmentSimulation(currentPrediction + 1) = investmentSimulation(currentPrediction);
+        end
+    end
+
+    % plotting results
+    if (~isnan(predictedClose(currentPrediction)))   % there exists a prediction
+        yyaxis left
+        predClosePlot(currentPrediction) = plot(Date(currentPredictionDateIdx - 1 : currentPredictionDateIdx), [Close(currentPredictionDateIdx - 1), predictedClose(currentPrediction)], '-');
+        
+        isPredictionGood = sign(predictedClose(currentPrediction) - Open(currentPredictionDateIdx)) == sign(Close(currentPredictionDateIdx) - Open(currentPredictionDateIdx));
+        if isPredictionGood
+        % stock price increased/decreased as predicted
+            predClosePlot(currentPrediction).Color = 'g';
+            predictionInfo.good = predictionInfo.good + 1;
+        else
+        % the prediction was wrong :(
+            predClosePlot(currentPrediction).Color = 'r';
+            predictionInfo.bad = predictionInfo.bad + 1;
+        end
+        predClosePlot(currentPrediction).LineWidth = 0.3;
+        predClosePlot(currentPrediction).Marker = '.';
+        predClosePlot(currentPrediction).MarkerSize = 5;
+
+        % valid prediction -> MAPE
+        MAPE = MAPE + abs((Close(currentPredictionDateIdx) - predictedClose(currentPrediction)) / Close(currentPredictionDateIdx));
+    else
+    % prediction is NaN
+        predictionInfo.invalid = predictionInfo.invalid + 1;
+    end
+end
+
+% plotting investmentSimulation
+yyaxis right
+investSimPlot = plot(Date([predictionIndexes, (predictionIndexes(end) + 1)]), investmentSimulation, '-', 'Color', 'k');
+
+% adjust axis
+
+hold off
+title("Real vs predicted Close values")
+
+% recap
+disp("------------------------------")
+totalNumberOfPredictions = (prediction.bad + prediction.good);
+predictionRatio = 100 * totalNumberOfPredictions / predictionLength;
+correctPredictionRatio = 100 * prediction.good / totalNumberOfPredictions;
+fprintf("Total predictions: %d (%.2f%%)\nCorrect derivative: %d (%.2f%%)\nWrong derivative: %d (%.2f%%)\n", ...
+    totalNumberOfPredictions, predictionRatio, ...
+    prediction.good, ...
+    correctPredictionRatio, ...
+    prediction.bad, 100 * prediction.bad / totalNumberOfPredictions);
+
+% Calcolo MAPE
+MAPE = MAPE / totalNumberOfPredictions;
+fprintf("Mean Absolute Percentage Error (MAPE): %.2f%%\n", MAPE*100);
+
+% print md instruction for appending the train to the table
+fprintf("|%s", extractAfter(filename, "train/")) 
+fprintf("|%s|%s|%s|%d|%d|%d|%d|%s|%d|%.2f%%|%.2f%%|%.2f%%|your notes here\n", extractBefore(stock_name, ".mat"), llim_date, ulim_date, underlyingStates, mixturesNumber, latency, useDynamicEdges, startPred_date, predictionLength, predictionRatio, correctPredictionRatio, MAPE*100);
+
+
+
+
+
+
+
+
 
 
 
