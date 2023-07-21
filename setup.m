706e075acb7787743110f95d61f99e68e82ef7f3
Luigi Catello <62564969+louis1642@users.noreply.github.com>
2023-07-21 16:09:46 +0200

    added documentation to setup

--- setup.m
+++ setup.m
@@ -1,9 +1,22 @@
 function setup(volatile)
+% Description:
+% This function sets up the necessary paths for the execution of the stock 
+% market prediction project. It adds specific directories to the MATLAB 
+% path, enabling access to relevant datasets, libraries, and utilities
+% required for the project.
+% 
+% Usage:
+% - setup() initializes the required paths.
+%   - If no argument is provided, the paths are saved for future sessions.
+%   - If the argument is set to 1, the paths are modified only for the 
+%       current session.
+%
     addpath("datasets\mat\")
     addpath("libs")             
     addpath("libs\map")
-    if (nargin>0 && volatile~=1)
-        savepath
+    if (nargin > 0 && volatile==1)
+        disp("Path will be modified only for current session.");
     else
-        disp("Path will be modified only for current session");
+        savepath
+        disp("Path will be saved across different sessions.")
     end
